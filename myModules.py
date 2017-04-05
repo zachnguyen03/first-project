e6d3ec49686ec189be8f351e2442cf6662482679
zachnguyen03 <quanhwalker1999@gmail.com>
2017-04-05 23:24:31 +0700

    Create myModules.py

---
+++ myModules.py
@@ -0,0 +1,11 @@
+import random
+def c_to_f(Temp)
+  F = 9*Temp/5 + 32
+  return F
+
+
+Temp = input("What's your current temperature in Celsius Degree?: ")
+  If Temp < -273.15
+    print("This temperature cannot be converted into Fahrenheit!!!")
+  else
+    print(F)
